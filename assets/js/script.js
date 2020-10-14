8ea460fa1fc07b5448c965ef0b40cbd7f09dd197
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-13 23:58:20 -0500

    Improving the drag feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,6 +155,7 @@ var dropZoneDragHandler= function(event){
     var taskListEl = event.target.closest(".task-list");
     if(taskListEl){
       event.preventDefault();
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -174,6 +175,14 @@ var dropTaskHandler= function(event){
         statusSelectEl.selectedIndex = 2;
     }
     dropZoneEl.appendChild(draggableElement);
+    dropZoneEl.removeAttribute("style");
+};
+
+var dragLeaveHandler= function(event){
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+      taskListEl.removeAttribute("style");
+    }
 };
 
 formEl.addEventListener("submit", taskFormHandler);
@@ -181,3 +190,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
